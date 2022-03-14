32270107503b7775a92f27afbd781bc068c04c50
dongl <dl978916237@163.com>
2022-03-14 23:50:00 +0800

    user、meeting服务搭建

---
+++ api-user/src/main/java/com/dongl/apiuser/ApiUserApplication.java
@@ -0,0 +1,15 @@
+package com.dongl.apiuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUserApplication.class, args);
+    }
+
+}
---
+++ service-meeting/src/test/java/com/dongl/servicemeeting/ServiceMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongl.servicemeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
